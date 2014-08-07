a502432a6fdffb7584b107460c96e3bc6f2cd8d3
Matt Garnes <matt@cyngn.com>
2014-08-06 17:55:15 -0700

    Avert confusion by renaming DataCard to CardData.
    Massive confusion has been reported by many, due to the fact that the crucial
    object backing DataCards is also called DataCard. Which is also not to
    be confused with any other type of card. Cards, cards, cards...
    
    SO... bite the bullet and rename all of the many instances of *Data*Card*
    to CardData, as this object represents the data that backs an actual card.
    
    Change-Id: I17a035cfd56c46f04da339b143a6289518b1deb7

--- src/main/java/org/cyanogenmod/launcher/cards/ApiCard.java
+++ src/main/java/org/cyanogenmod/launcher/cards/ApiCard.java
@@ -4,62 +4,62 @@ import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.Card.OnUndoSwipeListListener;
 
 import org.cyanogenmod.launcher.cardprovider.CmHomeApiCardProvider;
-import org.cyanogenmod.launcher.home.api.cards.DataCard;
+import org.cyanogenmod.launcher.home.api.cards.CardData;
 
 import android.content.Context;
 
 public class ApiCard extends Card implements OnUndoSwipeListListener {
 
-    private DataCard mDataCard;
+    private CardData mCardData;
 
-    public ApiCard(Context context, DataCard dataCard) {
+    public ApiCard(Context context, CardData cardData) {
         super(context);
-        init(dataCard);
+        init(cardData);
     }
 
-    public ApiCard(Context context, int innerLayout, DataCard dataCard) {
+    public ApiCard(Context context, int innerLayout, CardData cardData) {
         super(context, innerLayout);
-        init(dataCard);
+        init(cardData);
     }
 
-    private void init(DataCard dataCard) {
-        mDataCard = dataCard;
+    private void init(CardData cardData) {
+        mCardData = cardData;
         setSwipeable(true);
         setOnUndoSwipeListListener(this);
-        if (dataCard != null) {
-            setId(dataCard.getGlobalId());
+        if (cardData != null) {
+            setId(cardData.getGlobalId());
         }
     }
 
     public void setApiAuthority(String authority) {
-        mDataCard.setAuthority(authority);
+        mCardData.setAuthority(authority);
     }
 
     public String getApiAuthority() {
-        return mDataCard.getAuthority();
+        return mCardData.getAuthority();
     }
 
     public long getDbId() {
-        return mDataCard.getId();
+        return mCardData.getId();
     }
 
-    public void updateFromDataCard(DataCard dataCard) {
-        mDataCard = dataCard;
-        if (dataCard != null) {
-            setId(dataCard.getGlobalId());
+    public void updateFromCardData(CardData cardData) {
+        mCardData = cardData;
+        if (cardData != null) {
+            setId(cardData.getGlobalId());
         }
     }
 
-    public DataCard getDataCard() {
-        return mDataCard;
+    public CardData getCardData() {
+        return mCardData;
     }
 
     @Override
     public void onUndoSwipe(Card card, boolean timedOut) {
-        if (mDataCard != null && timedOut) {
-            boolean deleted = mDataCard.unpublish(mContext);
+        if (mCardData != null && timedOut) {
+            boolean deleted = mCardData.unpublish(mContext);
             if (deleted) {
-                CmHomeApiCardProvider.sendCardDeletedBroadcast(mContext, mDataCard);
+                CmHomeApiCardProvider.sendCardDeletedBroadcast(mContext, mCardData);
             }
         }
     }
